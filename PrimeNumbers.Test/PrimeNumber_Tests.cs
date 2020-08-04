65a74b8fed7f0617328921c38e3e17018ee35e63
Brendan Burch <44724846+Brendan525@users.noreply.github.com>
2020-08-04 09:05:05 -0400

    Added test files

---
+++ PrimeNumbers.Test/PrimeNumber_Tests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PrimeNumbers.Test
+{
+    public class PrimeNumber_Tests
+    {
+      [Fact]
+        public void TestPrime1()
+        {
+            PrimeNumbers numbers = new PrimeNumbers();
+            Assert.Equal(numbers.GetNPrime(1), 2);
+        }
+
+        [Fact]
+        public void TestPrime2()
+        {
+            PrimeNumbers numbers = new PrimeNumbers();
+            Assert.Equal(numbers.GetNPrime(2), 3);
+        }
+
+        [Fact]
+        public void TestPrime3()
+        {
+            PrimeNumbers numbers = new PrimeNumbers();
+            Assert.Equal(numbers.GetNPrime(3), 5);
+        }
+
+        [Fact]
+        public void TestPrime4()
+        {
+            PrimeNumbers numbers = new PrimeNumbers();
+            Assert.Equal(numbers.GetNPrime(4), 7);
+        }
+
+        [Fact]
+        public void TestPrime5()
+        {
+            PrimeNumbers numbers = new PrimeNumbers();
+            Assert.Equal(numbers.GetNPrime(5), 11);
+        }
+
+        [Fact]
+        public void TestPrime6() // Not suppose to work
+        {
+            PrimeNumbers numbers = new PrimeNumbers();
+            Assert.Equal(numbers.GetNPrime(6), 1);
+        }
+    }
+}
