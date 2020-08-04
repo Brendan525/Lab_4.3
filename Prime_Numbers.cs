e7f364587ab981168bbc909ff104996fbe23cfc6
Brendan Burch <44724846+Brendan525@users.noreply.github.com>
2020-08-03 22:13:06 -0400

    Add project files.

---
+++ Prime_Numbers.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimeNumbers
+{
+    public class PrimeNumbers
+    {
+
+        public int GetNPrime(int number)
+        {
+            List<int> primeNumbers = new List<int>();
+            primeNumbers.Add(2);
+            primeNumbers.Add(3);
+            primeNumbers.Add(5);
+            primeNumbers.Add(7);
+            primeNumbers.Add(11);
+            primeNumbers.Add(13);
+            primeNumbers.Add(17);
+            primeNumbers.Add(19);
+            primeNumbers.Add(23);
+            primeNumbers.Add(29);
+            primeNumbers.Add(31);
+            primeNumbers.Add(37);
+            primeNumbers.Add(41);
+            primeNumbers.Add(43);
+            primeNumbers.Add(47);
+            primeNumbers.Add(53);
+
+            return primeNumbers[number-1];
+
+            //example: input = 5    // 2, 3, 5, 7, 11
+            //take a number n ()the parameter
+            //get the nth prime number
+            //send it back
+
+            //there is no input, it is given as number
+            //there is no console writing, a number is returned
+        }
+
+
+        //public bool GetPrime(int n)
+        //{          
+
+        //    if (n % 2 == 0 && n != 2)
+        //    {
+        //        return false; // false
+        //    }
+        //    else
+        //    {
+        //        return true;
+        //    }
+            
+        //}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrimeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's locate some primes!");
+
+            Console.WriteLine("This application will find you any prime, in order, from first prime number on.");
+
+            while (true)
+            {
+                Console.WriteLine("Which prime number are you looking for?");
+
+                int userInput = Convert.ToInt32(Console.ReadLine());
+
+                PrimeNumbers prime = new PrimeNumbers();
+                Console.WriteLine($"The number {userInput} prime is {prime.GetNPrime(userInput)}");
+
+                Console.WriteLine("Do you want to find another prime number? (y/n): ");
+
+                string continueInput = Console.ReadLine().ToLower();
+
+                if (continueInput == "y")
+                {
+                    continue;
+                }
+                else if (continueInput == "n")
+                {
+                    Console.WriteLine("Goodbye!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter (y/n)");
+                }
+            }
+        }
+    }
+}
